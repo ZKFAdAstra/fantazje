6119ba7f9efb47293f6dd590f2926df809fce028
Michal Trojanowski <michal@flep.pl>
2015-03-01 20:27:37 +0100

    New rules for 2015 edition.

--- regulamin.php
+++ regulamin.php
@@ -1,25 +1,24 @@
-<h1>Fantazje Zielonogórskie 2013</h1>
+<h1>Fantazje Zielonogórskie 2015</h1>
 <p style="margin-top: 25px">
-    Zielonogórski Klub Fantastyki Ad Astra ogłasza III edycję otwartego konkursu 
+    Zielonogórski Klub Fantastyki Ad Astra ogłasza V edycję otwartego konkursu 
     literackiego Fantazje Zielonogórskie na opowiadanie fantastyczne inspirowane 
     przeszłością, teraźniejszością lub przyszłością Zielonej Góry. Prace powinny 
     być osadzone w konwencji szeroko pojmowanej fantastyki (sci-fi, fantasy, horror, 
-    legendy miejskie, realizm magiczny, itp.) splecionej z wątkami zielonogórskiej 
-    historii lub codzienności. Najlepsze konkursowe opowiadania zebrane zostaną w 
-    postaci antologii.
+    legendy miejskie, realizm magiczny, itp.) splecionej z wątkami zielonogórskiej historii 
+    lub codzienności. Najlepsze konkursowe opowiadania zebrane zostaną w postaci antologii. 
 </p>
 <p>
-    Sądzimy, że wiedzę o Zielonej Górze i jej bogatej historii można upowszechniać 
-    również za pomocą fantastyki. Klimat i metafizykę miasta można budować także 
-    w świecie wyobraźni. Chcielibyśmy aby co roku o kolejny tomik rozrastała się 
-    biblioteczka zielonogórskich fantazji, marzeń i koszmarów.
+	Sądzimy, że wiedzę o Zielonej Górze i jej bogatej historii można upowszechniać również 
+	za pomocą fantastyki. Klimat i metafizykę miasta można budować także w świecie wyobraźni. 
+	Chcielibyśmy aby co roku o kolejny tomik rozrastała się biblioteczka zielonogórskich fantazji, 
+	marzeń i koszmarów. 
 </p>
 <h2 style="margin-top: 35px">REGULAMIN</h2>
 <h3>I. Postanowienia ogólne</h3>
 <ol>
     <li>Organizatorem Konkursu jest Zielonogórski Klub Fantastyki Ad Astra, którego siedziba mieści się przy ul. Fabrycznej 13 B w Zielonej Górze.</li>
     <li>Sekretarzem Konkursu jest Maciej Dobrowolski.</li>
-    <li>Konkurs zostanie przeprowadzony w dniach od 1 marca 2013 roku do 1 czerwca 2013 roku. Lista Laureatów Konkursu zostanie ogłoszona do końca lipca 2013 roku.</li>
+    <li>Konkurs zostanie przeprowadzony w dniach od 1 marca 2015 roku do 1 czerwca 2015 roku. Lista Laureatów Konkursu zostanie ogłoszona do końca lipca 2015 roku.</li>
     <li>Przesłanie zgłoszenia na Konkurs jest jednoznaczne z akceptacją niniejszego Regulaminu.</li>
     <li>Przesłanie zgłoszenia na Konkurs jest jednoznaczne z wyrażeniem zgody na przetwarzanie przez Organizatora danych osobowych na potrzeby Konkursu (Ustawa o ochronie danych osobowych z dn. 29.08.1997 r. (tj. Dz. U. Nr 101 z 2002 r., poz. 926 z późn. zm.).</li>
     <li>Prace muszą być napisane w języku polskim.</li>
@@ -32,36 +31,35 @@
     <li>Konkurs jest otwarty dla wszystkich chętnych uczestników.</li>
     <li>Osoby niepełnoletnie mogą wziąć udział w Konkursie za zgodą rodziców lub przedstawicieli ustawowych przesyłając wraz z pracą skan zgody opiekuna.</li>
     <li>Uczestnik Konkursu może przysłać pracę wyłącznie taką, która jest wytworem jego oryginalnej twórczości, co oznacza, że praca nie może stanowić plagiatu, być kopią innych utworów lub ich fragmentów. Praca nie może być wcześniej publikowana i nagradzana - dotyczy to także publikacji internetowych.</li>
-    <li>Gotowe opowiadanie należy nadesłać na adres: fantazje@adastra.zgora.pl w formacie tekstowym (doc, rtf, odt), w tytule maila wpisując „Konkurs literacki Fantazje Zielonogórskie”. Objętość pracy to maksimum 35 tys. znaków (bez spacji).</li>
+    <li>Gotowe opowiadanie należy nadesłać na adres: fantazjekonkurs@gmail.com w formacie tekstowym (doc, rtf, odt), w tytule maila wpisując „Konkurs literacki Fantazje Zielonogórskie”. Objętość pracy to maksimum 35 tys. znaków (bez spacji).</li>
 </ol>
 <p></p>
 <h3>III. Zasady konkursu</h3>
 <ol>
-    <li>Aby wziąć udział w Konkursie należy spełniać wymagania określone w niniejszym Regulaminie. Prace można nadsyłać do dnia 1 czerwca 2013 roku włącznie.</li>
+    <li>Aby wziąć udział w Konkursie należy spełniać wymagania określone w niniejszym Regulaminie. Prace można nadsyłać do dnia 1 czerwca 2015 roku włącznie.</li>
     <li>W treści zgłoszenia należy podać: imię i nazwisko, adres autora oraz krótką notkę biograficzną. Wymagany jest również telefon kontaktowy oraz oświadczenie o posiadaniu autorskich praw majątkowych do pracy, o treści: "Oświadczam, iż nadesłana praca jest wynikiem wyłącznie mojej indywidualnej twórczości, nie stanowi plagiatu prac innych autorów".</li>
     <li>Każdy z uczestników Konkursu może nadesłać wyłącznie jedną pracę.</li>
     <li>Oceny nadesłanych prac dokona jury powołane przez Organizatora.</li>
     <li>Najlepsze prace, po wyborze dokonanym przez jury Konkursu, ukażą się w postaci publikacji książkowej (jako zbiór opowiadań).</li>
     <li>Decyzje Jury Konkursu są ostateczne.</li>
-    <li>Lista laureatów, których prace zostaną umieszczone w publikacji zostanie ogłoszona na stronie fantazje.adastra.zgora.pl do końca lipca 2013 roku.</li> 
+    <li>Lista laureatów, których prace zostaną umieszczone w publikacji zostanie ogłoszona na stronie fantazje.adastra.zgora.pl do końca lipca 2015 roku.</li> 
 </ol>
-
 <h3>IV. Nagrody</h3>
 <ol>
     <li>Wszyscy laureaci otrzymają po jednym egzemplarzu autorskim antologii.</li>
-    <li>Zwycięzca Konkursu otrzyma nagrodę pieniężną w wysokości 700 zł.</li>
+    <li>Zwycięzca Konkursu otrzyma nagrodę pieniężną w wysokości 1000 zł.</li>
     <li>O wygranej laureaci zostaną poinformowani drogą elektroniczną.</li>
-    <li>Nagroda Główna zostanie wręczona w trakcie Bachanaliów Fantastycznych 2013 w Zielonej Górze (wrzesień 2013) bądź w przypadku nieobecności zwycięzcy na konwencie, nagroda zostanie przekazana za pośrednictwem poczty lub innego przewoźnika, pod adres podany w zgłoszeniu konkursowym.</li>
+    <li>Nagroda Główna zostanie wręczona w trakcie Bachanaliów Fantastycznych 2015 w Zielonej Górze (wrzesień 2015) bądź w przypadku nieobecności zwycięzcy na konwencie, nagroda zostanie przekazana za pośrednictwem poczty lub innego przewoźnika, pod adres podany w zgłoszeniu konkursowym.</li>
     <li>Przyznana nagroda nie podlega wymianie na inną lub jakikolwiek ekwiwalent. </li>
 </ol>
 <p></p>
 <h3>V. Terminy</h3>
 <ul>
-    <li>1 marca 2013 – ogłoszenie Konkursu w mediach tradycyjnych i elektronicznych</li>
-    <li>do 1 czerwca 2013 - przyjmowanie prac</li>
-    <li>w dniach 20 czerwca - 15 lipca 2013 - posiedzenie jury i wyłonienie najlepszych prac</li>
-    <li>do końca lipca 2013 - ogłoszenie wyników obrad jury</li>
-    <li>wrzesień 2013 - Wręczenie głównej nagrody podczas XXVII Zielonogórskiego Festiwalu Fantastyki „Bachanalia Fantastyczne” 2013</li> 
+    <li>1 marca 2015 – ogłoszenie Konkursu w mediach tradycyjnych i elektronicznych</li>
+    <li>do 1 czerwca 2015 - przyjmowanie prac</li>
+    <li>w dniach 20 czerwca - 15 lipca 2015 - posiedzenie jury i wyłonienie najlepszych prac</li>
+    <li>do końca lipca 2015 - ogłoszenie wyników obrad jury</li>
+    <li>wrzesień 2015 - Wręczenie głównej nagrody podczas XXIX Zielonogórskiego Festiwalu Fantastyki „Bachanalia Fantastyczne” 2015</li> 
 </ul>
 <p></p>
 <h3>VI. Postanowienia końcowe</h3>
