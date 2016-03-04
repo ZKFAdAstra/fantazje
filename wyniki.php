2d62c47b68b1cd1170cbd93a7bef6b0b109d80de
Michal Trojanowski <michal@flep.pl>
2016-03-04 21:10:47 +0100

    Uaktolniony rok w wynikach.

--- wyniki.php
+++ wyniki.php
@@ -1,17 +1,18 @@
-<h2>Znamy zwycięzcę IV edycji konkursu Fantazje Zielonogórskie!</h2>
-
 <p>
-Zielonogórski Klub Fantastyki Ad Astra ogłosił w tym roku IV edycję otwartego konkursu literackiego Fantazje Zielonogórskie na opowiadanie fantastyczne inspirowane przeszłością, teraźniejszością lub przyszłością Zielonej Góry. Zainteresowanie naszą inicjatywą już tradycyjnie było znaczne, łącznie otrzymaliśmy 75 opowiadań nadesłanych z całej Polski. Jury przeczytało wszystkie prace i po licznych naradach wydało werdykt. Z wielką przyjemnością chciałbym ogłosić, iż zwycięzcą w tym roku została <strong>Dagmara Adwentowska</strong>, która w swoim opowiadaniu <em>„Trojnóg Diabła”</em>, urzekła kapitułę niebagatelną historią utrzymaną w klimatach dawnych opowieści o diabłach i czartach. Pani Dagmara oprócz lauru zwycięzcy otrzyma główną nagrodę o wartości tysiąca złotych. Serdecznie gratulujemy! 
+Z wielką przyjemnością możemy ogłosić, iż zwycięzcą w 2015 roku został <strong>Marek Starosta</strong>, który w swoim opowiadaniu <em>„Bóg, który przychodzi”</em>, urzekł kapitułę świetną klimatyczną historią osadzoną w latach 30. ubiegłego wieku. Tajne kulty, Bachus, polski ksiądz oraz niemiecka agentka, ten tekst po prostu porywa czytelnika i zabiera w niezapomnianą przygodę.
+</p> 
+<p>
+Panu Markowi serdecznie dziękujemy, szczególnie iż jest autorem, którego teksty można przeczytać w poprzednich edycjach Fantazji Zielonogórskich.  Zwycięzca otrzyma od nas główną nagrodę o wartości tysiąca złotych. Gratulujemy!
 </p>
 <p>
-Jednocześnie kapituła wyróżniła dwie prace, które jej zdaniem były również bardzo ciekawe i poważnie zagroziły zwycięzcy. Jest to <em>„Dzień Kobiet”</em> autorstwa Anny Konstanty oraz <em>„Rechot”</em> Aleksandra Daukszewicza. Obydwoje twórców otrzyma od nas nagrody niespodzianki.
+Jednocześnie kapituła wyróżniła dwie prace, które jej zdaniem były również bardzo ciekawe i stanowiły nie małą konkurencję dla zwycięzcy. Jest to <em>„Król Szczupak”</em> napisany przez <strong>Monikę Kasprowiak </strong> oraz <em>„Grünberg.One”</em>, którego autorem jest <strong>Adam Froń</strong>.
 </p>
 <p>
-Pozostałe prace, które ze względu na swój wysoki poziom znajdą się w książkowej publikacji naszej antologii konkursowej to: <br/> 
-<em>„Sed libera nos a malo”</em> - Iwona Przybylska, <em>„Przeskok”</em> - Marek Palutkiewicz, <em>„Biały rycerz”</em> - Michał Organiściak, <em>„Stalowy Żółw”</em> - Krzysztof Nowecki, <em>„Wydarzenia”</em> – Filip Kopij, <em>„Zaręczyny”</em> - Żaneta Siemsia, <em>„Atak serca”</em> – Alfred Siatecki, <em>„Święto Złotego Wina”</em> – Tadeusz Kajak, <em>„Dziwka generała”</em> - Kamila Modrzyńska
+Pozostałe prace, które ze względu na swój wysoki poziom znajdą się w książkowej publikacji naszej antologii konkursowej to:<br/> 
+<em>"Miasto Srok"</em> - Dominika Hejduk, <em>"Saudade Pana"</em> - Filip Koziński, <em>"Zbieracz Pijanych Hansów"</em> - Krzysztof Gniady, <em>"Było jutro"</em> - Marta Magdalena Lasik, <em>"Af Rika"</em> - Marek Zychla, <em>"Nihil fit sine causa"</em> - Dorota Dziedzic-Chojnacka, <em>"Czego należało dowieść"</em> - Marta Krajewska, <em>"Sroki z Wieży Głodowej"</em> - Alicja Tempłowicz oraz <em>"Familariusz" </em>- Marek Palutkiewicz
 </p>
 <p>
-Książka pt. Fantazje Zielonogórskie tom IV swoją premierę będzie miała podczas tegorocznych Bachanaliów Fantastycznych odbywających się w dniach 19-21 września w Zielonej Górze. Wtedy też wręczone zostaną wyróżnienia oraz nagroda główna. Chcielibyśmy również serdecznie podziękować wszystkim uczestnikom naszej zabawy za udział w konkursie. 
+Książka pt. <em>Fantazje Zielonogórskie tom V</em> swoją premierę będzie miała podczas tegorocznych Bachanaliów Fantastycznych odbywających się w dniach 18-20 września w Zielonej Górze. Chcielibyśmy również serdecznie podziękować wszystkim uczestnikom naszej zabawy za udział w konkursie.
 </p>
 	
 <p class="podpis">
